4ae2d5eb7015ea8bb09a62bf540f13c4a0a5794b
theonlyharsh <creatorzuni@gmail.com>
2025-02-23 03:28:16 +0000

    Update app.py
    number to digit changed remotely

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = float(input("Enter first digit: "))
+num2 = float(input("Enter second digit: "))
 print(f"Sum: {num1 + num2}")
